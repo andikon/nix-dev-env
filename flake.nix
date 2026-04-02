b8a43499daf3b2d0d3982d2d4aed066fcb871fe2
Andrij König <47005148+andikon@users.noreply.github.com>
2026-04-02 09:05:46 +0200

    feat: readd dotfiles

--- flake.nix
+++ flake.nix
@@ -59,14 +59,29 @@
         # Container root FS
         # ------------------------
 
+        # Add basic utilities and dotfiles for the image
         imageRoot = pkgs.buildEnv {
           name = "image-root";
           paths = [
             userEnv
             pkgs.bash
             pkgs.fish
-          ];
+            pkgs.coreutils    # mkdir, rm, cp, etc
+            pkgs.util-linux   # uname
+            pkgs.findutils    # find
+            pkgs.gnugrep      # grep
+            pkgs.gnused       # sed
+            pkgs.gawk         # awk
+          ] ++ [ dotfiles ];  # copy dotfiles into /nix/store/imageRoot/dotfiles
         };
+
+        # Wrap dotfiles copy to /root inside container
+        imageRootWithDotfiles = pkgs.runCommand "dotfiles-root" { } ''
+          mkdir -p $out/root
+          cp -r ${dotfiles}/* $out/root/
+          # Merge with previous imageRoot
+          cp -r ${imageRoot}/* $out/
+        '';
 
         # ------------------------
         # nix2container image
@@ -77,7 +92,8 @@
             name = "docker.io/andrijkoenig/nix-dev-env";
             tag = "latest";
 
-            copyToRoot = imageRoot;
+            # Use the combined root FS
+            copyToRoot = imageRootWithDotfiles;
 
             config = {
               Cmd = [ "${pkgs.fish}/bin/fish" ];
@@ -88,13 +104,10 @@
 
       in
       {
-        packages =
-          {
-            default = userEnv;
-          }
-          // lib.optionalAttrs pkgs.stdenv.isLinux {
-            devContainer = devContainer;
-          };
+        packages = lib.optionalAttrs pkgs.stdenv.isLinux {
+          default = userEnv;
+          devContainer = devContainer;
+        };
 
         devShells.default = pkgs.mkShell {
           packages = packagesList;
