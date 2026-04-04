83e4407c04f881f5d22f4f6314e7a0378f6c9ea5
Andrij König <47005148+andikon@users.noreply.github.com>
2026-04-04 23:05:49 +0200

    add fakeNss

--- flake.nix
+++ flake.nix
@@ -12,6 +12,17 @@
       pkgs = import nixpkgs { inherit system; };
 	  
 	  home = dotfiles.homeConfigurations."dev@docker".activationPackage;
+	  
+	  fakeNssDev =
+		  pkgs.dockerTools.fakeNss.override {
+			extraPasswdLines = [
+			  "dev:x:1000:1000:Dev User:/home/dev:${pkgs.fish}/bin/fish"
+			];
+
+			extraGroupLines = [
+			  "dev:x:1000:"
+			];
+		  };
     in {
       packages.${system}.dev-docker = pkgs.dockerTools.buildLayeredImage  {
         name = "nix-dev-env";
@@ -19,6 +30,7 @@
 		
 		contents = [
 		  home
+		  fakeNssDev
 		] ++ (with pkgs; [
 		  coreutils  
 		  busybox
@@ -37,14 +49,24 @@
         ]);
 
         extraCommands = ''
-		  mkdir -p home/dev
-		  chown -R 1000:1000 home/dev
-		'';
+          # create home directory
+          # mkdir -p home/dev
+
+          # install home-manager result
+          # cp -r ${home}/home-files/. home/dev/
+
+          # ownership by numeric uid/gid
+          # chown -R 1000:1000 home/dev
+
+          # fish expects /bin/sh
+          # mkdir -p bin
+          # ln -s ${pkgs.bash}/bin/bash bin/sh
+        '';
 
         config = {
           User = "1000:1000";
           Cmd = [ "${pkgs.fish}/bin/fish" "-l" ];
-          Env = [ "HOME=/home/dev" ];
+          Env = [ "HOME=/home/dev" "USER=dev" ];
           WorkingDir = "/home/dev";
         };
       };
