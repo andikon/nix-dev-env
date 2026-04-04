b24e5003b58ff87aaf6c7db8f35d86e02a5f13ab
Andrij König <47005148+andikon@users.noreply.github.com>
2026-04-04 17:08:43 +0200

    feat: update nix flake

--- flake.nix
+++ flake.nix
@@ -1,115 +1,43 @@
 {
-  description = "Custom Development Environment Docker Image";
+  description = "dev docker image with dotfiles (built with nix)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    
-    # Fetch your dotfiles directly via flake inputs
-    dotfiles = {
-      url = "github:andikon/dotfiles";
-      flake = false;
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    dotfiles.url = "github:andikon/dotfiles";
   };
 
-  outputs = { self, nixpkgs, dotfiles }:
+  outputs = { self, nixpkgs, dotfiles, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-
-      # User definitions
-      user = "dev";
-      uid = "1000";
-      gid = "1000";
-      home = "/home/${user}";
-
-      # Pre-bake the home directory by running chezmoi during the Nix build
-      bakedHome = pkgs.stdenv.mkDerivation {
-        name = "dev-home";
-        src = dotfiles;
-        nativeBuildInputs = [ pkgs.chezmoi pkgs.git ];
-        
-        buildPhase = ''
-          # Create a temporary home directory for the build
-          export HOME=$PWD/home
-          export USER=${user}
-          mkdir -p $HOME
-
-          # Apply chezmoi using the nested 'dotfiles' folder from your repo
-          chezmoi --source $src/dotfiles apply
-        '';
-        
-        installPhase = ''
-          mkdir -p $out
-          # Copy everything, including hidden files, to the output
-          cp -rT $HOME $out
-        '';
-      };
-
-    in
-    {
-      packages.${system}.default = pkgs.dockerTools.buildLayeredImage {
+    in {
+      packages.${system}.dev-docker = pkgs.dockerTools.buildImage {
         name = "nix-dev-env";
         tag = "latest";
-        created = "now"; # Ensures the image has a timestamp, overriding the 1970 Nix epoch
+        fromImage = "docker://ubuntu:22.04";
 
         contents = with pkgs; [
-          bashInteractive
-          fish
-          git
           neovim
           tmux
-          ripgrep
-          fd
-          fzf
-          sudo
-          coreutils
-          shadow
-          chezmoi
+          git
+          wget
+          curl
+          openjdk
+          nodejs
+          fish
+          typescript-language-server
         ];
 
-        # fakeRootCommands allows us to manipulate the file system as 'root' 
-        # before the final image layers are squashed.
-        fakeRootCommands = ''
-          # Set up standard directories
-          mkdir -p tmp
-          chmod 1777 tmp
-
-          # Link common shell and env paths heavily relied upon by scripts
-          mkdir -p usr/bin bin
-          ln -s ${pkgs.coreutils}/bin/env usr/bin/env
-          ln -s ${pkgs.bashInteractive}/bin/bash bin/sh
-
-          # Set up /etc/passwd, /etc/group, /etc/shadow manually
-          mkdir -p etc
-          echo "root:x:0:0:root:/root:${pkgs.bashInteractive}/bin/bash" > etc/passwd
-          echo "${user}:x:${uid}:${gid}:Dev User:${home}:${pkgs.fish}/bin/fish" >> etc/passwd
-
-          echo "root:x:0:" > etc/group
-          echo "${user}:x:${gid}:" >> etc/group
-
-          echo "root:!x:::::::" > etc/shadow
-          echo "${user}:!x:::::::" >> etc/shadow
-
-          # Configure passwordless sudo for the dev user
-          mkdir -p etc/sudoers.d
-          echo "${user} ALL=(ALL) NOPASSWD: ALL" > etc/sudoers.d/${user}
-          chmod 0440 etc/sudoers.d/${user}
-
-          # Inject the baked home directory and fix ownership
-          mkdir -p ${home}
-          cp -rT ${bakedHome} ${home}
-          chown -R ${uid}:${gid} ${home}
+        runAsRoot = ''
+          ${pkgs.dockerTools.shadowSetup}
+          useradd -m -s ${pkgs.fish}/bin/fish dev
         '';
 
         config = {
-          Cmd = [ "${pkgs.fish}/bin/fish" ];
-          User = user;
-          Env = [
-            "USER=${user}"
-            "HOME=${home}"
-            "TERM=xterm-256color"
-          ];
-          WorkingDir = home;
+          User = "dev";
+          Cmd = [ "${pkgs.fish}/bin/fish" "-l" ];
+          Env = [ "HOME=/home/dev" ];
+          WorkingDir = "/home/dev";
         };
       };
     };
